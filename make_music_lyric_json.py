bf44710ef8928766c15d70da0e79177552d55aff
n3xem <yukyanjpn@gmail.com>
2020-07-28 00:43:53 +0900

    Add: 歌詞付の音楽リストjson作成機能

---
+++ make_music_lyric_json.py
@@ -0,0 +1,53 @@
+import requests
+import re
+import time
+from bs4 import BeautifulSoup
+from janome.tokenizer import Tokenizer
+import json
+
+
+def download_lyric(lyricTitle):
+    lyricSearchUrl = "https://www.uta-net.com/search/?Aselect=2&Bselect=4&Keyword={0}&sort=4".format(
+        lyricTitle)
+    response = requests.get(lyricSearchUrl)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    lyricUrlComp = soup.select(
+        "tbody:first-of-type tr:first-of-type td:first-of-type a")
+    if lyricUrlComp == []:
+        return "none"
+    lyricUrl = "https://www.uta-net.com" + lyricUrlComp[0].attrs['href']
+    response = requests.get(lyricUrl)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    lyricHtml = str(soup.find("div", id="kashi_area"))
+    lyric = re.sub('<.+?>', ' ', lyricHtml)
+    return lyric
+
+
+def create_music_dic(orig_dic):
+    music_title = orig_dic["name"]
+    artist_name = orig_dic["artistName"]
+    genre = orig_dic["genres"][0]["name"]
+    lyric = download_lyric(music_title)
+
+    table = str.maketrans({
+        '\u3000': ''
+    })
+
+    lyric = lyric.translate(table)
+    time.sleep(1)
+    procced_dic = {'music_title': music_title,
+                   'artist_name': artist_name, 'genre': genre, 'lyric': lyric}
+    return procced_dic
+
+
+dic = {}
+with open('./explicit.json') as f:
+    musicJson = json.load(f)
+    for i in range(100):
+        procced_dic = create_music_dic(musicJson["feed"]["results"][i])
+        dic.update({i: procced_dic})
+    print(dic)
+
+with open('./music_lyric.json', 'w') as f:
+    json.dump(dic, f, ensure_ascii=False, indent=4)
